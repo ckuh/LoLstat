941e00d2f595e8002305d01b18185d5c840e3eac
Chris Kuh <kuh_chris@yahoo.com>
2016-03-12 07:00:01 -0800

    [refactor] clean console log

--- server/helperFunc/getUserDataHelper.js
+++ server/helperFunc/getUserDataHelper.js
@@ -2,7 +2,6 @@ module.exports = {
   summonerName: function(riot, userName, callback) {
     getSummonerInfo(riot, userName, function(err, summonerInfo) {
       if (err) {
-        console.error('+++line5 :', err);
         callback(summonerInfo);
       } else {
         callback(summonerInfo);
@@ -13,12 +12,10 @@ module.exports = {
   summonerLeague: function(riot, userName, callback) {
     getSummonerInfo(riot, userName, function(err, summonerInfo) {
       if (err) {
-        console.error('+++line16 :', err);
         callback(summonerInfo);
       } else {
         riot.league.bySummonerEntry(summonerInfo.id, {}, function(err, data) {
           if (err) {
-            console.error('+++line21 :', err);
             callback(JSON.parse(data).status);
           } else {
             callback(data[summonerInfo.id][0]);
@@ -29,15 +26,12 @@ module.exports = {
   },
 
   statsSummary: function(riot, userName, callback) {
-    console.log('+++line3 userName: ', userName);
     getSummonerInfo(riot, userName, function(err, summonerInfo) {
       if (err) {
-        console.error('+++line 35:', err);
         callback(summonerInfo);
       } else {
         riot.stats.summary(summonerInfo.id, {}, function(err, data) {
           if (err) {
-            console.error('+++line 40:', err);
             callback(JSON.parse(data).status);
           } else {
             callback(data);
@@ -50,14 +44,12 @@ module.exports = {
   statsRanked: function(riot, userName, season, callback) {
     getSummonerInfo(riot, userName, function(err, summonerInfo) {
       if (err) {
-        console.error('+++line 53:', err);
         callback(summonerInfo);
       } else {
         riot.stats.ranked(summonerInfo.id, {
           season: season
         }, function(err, data) {
           if (err) {
-            console.error('+++line 60:', err);
             callback(JSON.parse(data).status);
           } else {
             callback(data);
@@ -69,10 +61,8 @@ module.exports = {
 }
 
 function getSummonerInfo(riot, userName, callback) {
-  console.log(userName);
   riot.summoner.byName(userName, {}, function(err, data) {
     if (err) {
-      console.error('+++line 75:', err);
       callback(err, JSON.parse(data).status);
     } else {
       callback(err, data[userName]);
